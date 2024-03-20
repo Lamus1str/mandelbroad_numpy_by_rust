1d2206d49fded2364c59a61664625dab8150ac6c
Polyzhan <almazovi2000@gmail.com>
2024-03-20 11:24:00 +0700

    .

--- src/main.rs
+++ src/main.rs
@@ -55,9 +55,11 @@ fn get_color(n: usize, offset: f32) -> Color{
     //                (new_n) * 0.032 - 0.64)
 }
 
+const WIN_SIZE: (u32, u32) = (640, 640);
+
 fn main()
 {
-    let window = Window::new_centered("Mandelbrot's set", (600, 600)).unwrap();
+    let window = Window::new_centered("Mandelbrot's set", WIN_SIZE).unwrap();
 
     window.run_loop(MyWindowHandler {
         scale: 400.0,
@@ -83,7 +85,9 @@ impl WindowHandler for MyWindowHandler
 {
     fn on_mouse_move(&mut self, helper: &mut WindowHelper<()>, position: Vec2) {
         let pos_f64 = Vector2::<f64>::new(position.x as f64, position.y as f64);
-        self.mouse_pos = (pos_f64 - Vector2::<f64>::new(300.0, 300.0)) / self.scale as f64;
+        self.mouse_pos = (pos_f64 -
+            Vector2::<f64>::new(WIN_SIZE.0 as f64 / 2.0, WIN_SIZE.1 as f64 / 2.0))
+            / self.scale as f64;
     }
     fn on_mouse_button_down(&mut self, helper: &mut WindowHelper<()>, button: MouseButton) {
         //println!("{:?}", self.mouse_pos);
@@ -91,38 +95,48 @@ impl WindowHandler for MyWindowHandler
         helper.request_redraw();
     }
     fn on_key_down(&mut self, helper: &mut WindowHelper<()>, virtual_key_code: Option<VirtualKeyCode>, scancode: KeyScancode) {
+        let mut redraw = false;
         if let Some(key_code) = virtual_key_code{
             if key_code == VirtualKeyCode::A{
                 if self.mul_or_add{
                     self.degree *= 2;
+                    redraw = true;
                 }
                 else{
                     self.degree += 1;
+                    redraw = true;
                 }
             }
             else if key_code == VirtualKeyCode::S{
                 if self.mul_or_add{
                     self.degree /= 2;
+                    redraw = true;
                 }
                 else{
                     self.degree -= 1;
+                    redraw = true;
                 }
             }
             else if key_code == VirtualKeyCode::Equals{
                 println!("scale+");
                 self.scale *= 2.0;
+                redraw = true;
             }
             else if key_code == VirtualKeyCode::Minus {
                 self.scale *= 0.5;
                 println!("scale-");
+                redraw = true;
             }
             else if key_code == VirtualKeyCode::LControl {
                 self.mul_or_add = !self.mul_or_add;
+                redraw = true;
             }
             else{
                 return;
             }
-            helper.request_redraw();
+            if redraw{
+                helper.request_redraw();
+            }
         }
     }
     fn on_draw(&mut self, helper: &mut WindowHelper, graphics: &mut Graphics2D)
@@ -130,11 +144,12 @@ impl WindowHandler for MyWindowHandler
         self.drawed = true;
         graphics.clear_screen(Color::BLACK);
         println!("drawing");
-        for x in 0..600{
-            for y in 0..600{
+        for x in 0..WIN_SIZE.0{
+            for y in 0..WIN_SIZE.1{
                 let c = c32 {
-                    a: (x as f64 - 300.0) / self.scale + self.centre.x as f64,
-                    b: (y as f64 - 300.0) / self.scale + self.centre.y as f64};
+                    a: (x as f64 - WIN_SIZE.0 as f64 / 2.0) / self.scale + self.centre.x,
+                    b: (y as f64 - WIN_SIZE.1 as f64 / 2.0) / self.scale + self.centre.y
+                };
                 let mut z = c32 { a: 0.0, b: 0.0};
                 for n in 0..self.degree{
                     z = (z * z) + c;
